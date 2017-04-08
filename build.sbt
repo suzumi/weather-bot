2d3053036b0577aed231050fbf5b87822aed2105
suzumi <ganimata39@gmail.com>
2017-04-08 10:45:01 +0900

    add bot

---
+++ build.sbt
@@ -0,0 +1,21 @@
+import Dependencies._
+
+lazy val root = (project in file(".")).
+  settings(
+    inThisBuild(List(
+      organization := "com.example",
+      scalaVersion := "2.12.1",
+      version      := "0.1.0-SNAPSHOT"
+    )),
+
+    name := "Hello",
+
+    libraryDependencies ++= Seq(
+      scalaTest % Test,
+      "org.json4s" %% "json4s-native" % "3.5.0",
+      "org.skinny-framework" %% "skinny-http-client" % "2.3.3",
+      "joda-time" % "joda-time" % "2.9.2", // 使ってない
+      "org.joda" % "joda-convert" % "1.8",
+      "com.enragedginger" %% "akka-quartz-scheduler" % "1.6.0-akka-2.4.x"
+    )
+  )
---
+++ src/main/scala/example/Hello.scala
@@ -0,0 +1,71 @@
+package example
+
+import org.json4s._
+import org.json4s.JsonDSL._
+import org.json4s.native.JsonMethods._
+import skinny.http._
+import org.joda.time.{DateTime, DateTimeZone}
+import java.io.{ByteArrayInputStream, ByteArrayOutputStream, ObjectInputStream, ObjectOutputStream}
+
+
+
+object Hello {
+
+  implicit val formats = DefaultFormats
+
+  case class Forecast(
+    dateLabel: String,
+    telop: String,
+    date: String,
+    temperature: Temperature,
+    image: Image
+  )
+  case class Temp(celsius: String, fahrenheit: String)
+  case class Forecasts(forecasts: List[Forecast])
+  case class Temperature(min: Option[Temp], max: Option[Temp])
+  case class Image(width: Int, url: String, title: String, height: Int)
+  case class Weather(title: String, description: String)
+
+  def main(args: Array[String]): Unit = {
+    val aggregateForecastSummary: Seq[String] = Nil
+    val response = HTTP.get("http://weather.livedoor.com/forecast/webservice/json/v1", "city" -> 130010)
+    val json = parse(response.asString)
+
+    val descriptionJson = json \ "description" \ "text"
+    // サマリ
+    val description = s"*今日の天気概要*:\n${descriptionJson.extract[String]}"
+
+    val forecastsJson = (json \ "forecasts").extract[List[Forecast]]
+//    val forecasts = forecastsJson
+
+    // 予報
+    val forecastsList = forecastsJson.foldLeft(aggregateForecastSummary){ (acc, forecast) =>
+      val build = s"""
+        | *${forecast.dateLabel}*
+        | *${forecast.telop}*
+        | *最高気温：${forecast.temperature.max.map(x => x.celsius).getOrElse("")}℃*
+        | *最低気温：${forecast.temperature.min.map(x => x.celsius).getOrElse("")}℃*
+      """.stripMargin
+
+      acc :+ build
+
+    }
+    val forecasts = forecastsList.flatten
+    println(forecasts)
+
+    val url = "https://hooks.slack.com/services/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"
+    val reqJson = ("text" -> s"$forecasts\n$description")
+
+    HTTP.post(url, compact(render(reqJson)))
+
+  }
+
+  def serialise(value: Any): Array[Byte] = {
+    val stream: ByteArrayOutputStream = new ByteArrayOutputStream()
+    val oos = new ObjectOutputStream(stream)
+    oos.writeObject(value)
+    oos.close
+    stream.toByteArray
+  }
+
+}
